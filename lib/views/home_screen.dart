771eb7c752b2b1957d1fdbf56515d7339d91c1a9
muhammad.adnan <adii.developer277@gmail.com>
2022-04-17 13:00:59 +0500

    third commit

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -11,12 +11,66 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+List<Api_model> apiList = [];
+
+Future<List<Api_model>> getApiList() async {
+  final response =
+      await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+  var data = jsonDecode(response.body.toString());
+  if (response.statusCode == 200) {
+    for (Map i in data) {
+      apiList.add(Api_model.fromJson(i));
+    }
+    return apiList;
+  } else {
+    return apiList;
+  }
+}
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("API Example"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getApiList(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text("Loading...");
+                  } else {
+                    return ListView.builder(
+                        itemCount: apiList.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  child: Text(apiList[index].id.toString()),
+                                ),
+                                Text(
+                                  apiList[index].title.toString(),
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  apiList[index].body.toString(),
+                                  textAlign: TextAlign.justify,
+                                ),
+                              ],
+                            ),
+                          );
+                        });
+                  }
+                }),
+          ),
+        ],
       ),
     );
   }
